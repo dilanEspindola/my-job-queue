06832a7a690fb34e6124d96667a67d280f63c039
dilan <dilanespindola30@gmail.com>
2026-05-14 16:44:21 -0500

    Optimization to the worker - it added atomic values to syncronize active_threads, task_executed and pending_tasks - it added a loop inside the thread to fix thread execution limi:    By thread execution limi i mean: Before the thread was not able to run mutiple task. If thread pool was limit to X number And i have many task in the queue it won't run all the task for example if i have 50 task and 5 workers, the workers would just run 5 task due to the execution of the thread end. So that's why i added a loop inside   ``thread::spawn``

--- src/worker.rs
+++ src/worker.rs
@@ -1,6 +1,6 @@
 use crate::buffer::{Buffer, BufferTrait};
 use std::{
-    sync::{Arc, Mutex},
+    sync::{atomic::AtomicUsize, atomic::Ordering, Arc, Mutex},
     thread,
 };
 
@@ -10,22 +10,34 @@ impl Worker {
     pub fn new() -> Self {
         return Worker {};
     }
-    pub fn start(&self, buffer: Arc<Mutex<Buffer>>) -> thread::JoinHandle<()> {
+    pub fn start(
+        &self,
+        buffer: Arc<Mutex<Buffer>>,
+        active_threads: Arc<AtomicUsize>,
+        tasks_executed: Arc<AtomicUsize>,
+        tasks_pending: Arc<AtomicUsize>,
+    ) -> thread::JoinHandle<()> {
         thread::spawn(move || {
-            println!("Worker started");
+            active_threads.fetch_add(1, Ordering::Relaxed);
 
-            let task = {
-                let mut buffer = buffer.lock().unwrap();
-                buffer.remove()
-            };
+            loop {
+                let task = {
+                    let mut buffer = buffer.lock().unwrap();
+                    buffer.remove()
+                };
 
-            match task {
-                Some(task) => {
-                    let item = task.item;
-                    item();
+                match task {
+                    Some(task) => {
+                        let item = task.item;
+                        item();
+                        tasks_executed.fetch_add(1, Ordering::Relaxed);
+                        tasks_pending.fetch_sub(1, Ordering::Relaxed);
+                    }
+                    None => break,
                 }
-                None => println!("No tasks to execute"),
             }
+
+            active_threads.fetch_sub(1, Ordering::Relaxed);
         })
     }
 }
